989385cffc4de9868e58b600a64f9276949857d3
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-05-26 02:18:05 +0000

    .

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -208,7 +208,7 @@ async def upstream(event):
             remove("output.txt")
         else:
             await event.edit(changelog_str)
-        return await event.respond('**Perintah Untuk Update, Sebagai Berikut.**\n ⚡𝘾𝙈𝘿⚡: >`.update now`\n ⚡𝘾𝙈𝘿⚡: >`.update deploy`\n\n__Untuk Meng Update Fitur Terbaru Dari ⚡𝗟𝘆𝗻𝘅-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡.__')
+        return await event.respond('**Perintah Untuk Update, Sebagai Berikut :**\n ⚡𝘾𝙈𝘿⚡: `.update -pull`\n ⚡𝘾𝙈𝘿⚡: `.update -push`\n\n__Untuk Meng-Update Fiture Terbaru Dari ⚡𝗟𝘆𝗻𝘅-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡.__')
 
     if force_update:
         await event.edit(
