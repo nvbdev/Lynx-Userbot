23164e19af8691356a2bd62cdb005013bc92e320
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-06-13 12:19:11 +0000

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -448,24 +448,23 @@ with bot:
                 data=re.compile("open")
             )
         )
-        async def _(event):
-            buttons = [
-                (
-                    custom.Button.inline(
-                        "⋖╯Pʀᴇᴠ", data="helpme_prev")
-                    ),
-                    custom.Button.inline(
-                        "ᴄʟᴏꜱᴇ", data="helpme_close")
-                    ),
-                    custom.Button.inline(
-                    "Nᴇxᴛ╰⋗", data="helpme_next")
+        async def opeen(event):
+            try:
+                tgbotusername = BOT_USERNAME
+                if tgbotusername is not None:
+                    results = await event.client.inline_query(tgbotusername, "@LynxRobot")
+                    await results[0].click(
+                        event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
                     )
+                    await event.delete()
+                else:
+                    await event.edit(
+                        "`The bot doesn't work! Please set the Bot Token and Username correctly. The module has been stopped.`"
+                    )
+            except Exception:
+                return await event.edit(
+                    "`You cannot send inline results in this chat (caused by SendInlineBotResultRequest)`"
                 )
-            ]
-            await event.edit(
-                 buttons=buttons,
-                 link_preview=False,
-            )
 
 
         lynxlogo = "resource/logo/LynxUserbot-Button.jpg"
