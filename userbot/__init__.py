2b4e36a27b8e8f442423042383e3ff8a44d0bd47
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-06-14 10:05:58 +0000

    Alpha.

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -430,6 +430,42 @@ def paginate_help(page_number, loaded_modules, prefix):
     return pairs
 
 
+
+def opener_help(page_number, loaded_modules, prefix):
+    number_of_rows = 4
+    number_of_cols = 3
+    helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
+    helpable_modules = sorted(helpable_modules)
+    modules = [
+        custom.Button.inline("{} {} 」◑".format("◐「", x), data="ub_plugins_{}".format(x))
+        for x in helpable_modules
+    ]
+    pairs = list(zip(modules[::number_of_cols],
+                     modules[1::number_of_cols],
+                     modules[2::number_of_cols]))
+    if len(modules) % number_of_cols == 1:
+        pairs.append((modules[-1],))
+    max_num_pages = ceil(len(pairs) / number_of_rows)
+    modulo_page = page_number % max_num_pages
+    if len(pairs) > number_of_rows:
+        pairs = pairs[
+            modulo_page * number_of_rows: number_of_rows * (modulo_page + 1)
+        ] + [
+            (
+                custom.Button.inline(
+                    "⋖╯Pʀᴇᴠ", data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                custom.Button.inline(
+                    "ᴄʟᴏꜱᴇ", data="{}_close({})".format(prefix, modulo_page)
+                ),
+                custom.Button.inline(
+                    "Nᴇxᴛ╰⋗", data="{}_next({})".format(prefix, modulo_page)
+                )
+            )
+        ]
+    return pairs
+
+
 with bot:
     try:
         tgbot = TelegramClient(
@@ -452,7 +488,7 @@ with bot:
             try:
                 tgbotusername = BOT_USERNAME
                 if tgbotusername is not None:
-                    results = await event.client.inline_query(tgbotusername, "@LynxRobot")
+                    results = await event.client.inline_query(tgbotusername, "@LynxBot")
                     await results[0].click(
                         event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
                     )
@@ -467,13 +503,46 @@ with bot:
                 )
 
 
+        @tgbot.on(
+            events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+                data=re.compile(rb"ub_plugins_(.*)")
+            )
+        )
+        async def on_plug_in_callback_query_handler(event):
+            if event.query.user_id == uid:  # pylint:disable=E0602
+                modul_name = event.data_match.group(1).decode("UTF-8")
+
+                cmdhel = str(CMD_HELP[modul_name])
+                if len(cmdhel) > 150:
+                    help_string = (
+                        str(CMD_HELP[modul_name]).replace('`', '')[:150] + "..."
+                        + "\n\nBaca Text Berikutnya Ketik .help "
+                        + modul_name
+                        + " "
+                    )
+                else:
+                    help_string = str(CMD_HELP[modul_name]).replace('`', '')
+
+                reply_pop_up_alert = (
+                    help_string
+                    if help_string is not None
+                    else "{} No document has been written for module.".format(
+                        modul_name
+                    )
+                )
+            else:
+                reply_pop_up_alert = f"🚫!WARNING!🚫 Jangan Menggunakan Milik {DEFAULTUSER}."
+
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+
         @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
         async def inline_opeen(event):
             builder = event.builder
             result = None
             query = event.text
-            if event.query.user_id == uid and query.startswith("@LynxRobot"):
-                buttons = paginate_help(0, dugmeler, "open")
+            if event.query.user_id == uid and query.startswith("@LynxBot"):
+                buttons = opener_help(0, dugmeler, "open")
                 result = builder.photo(
                     file=lynxlogo,
                     link_preview=False,
@@ -506,8 +575,6 @@ with bot:
             await event.answer([result] if result else None)
 
 
-
-
         lynxlogo = "resource/logo/LynxUserbot-Button.jpg"
         plugins = CMD_HELP
 
@@ -528,6 +595,7 @@ with bot:
                                         "t.me/FederationSuperGroup/17")],
                     ]
                 )
+
 
         @tgbot.on(events.NewMessage(pattern="/deploy"))
         async def handler(event):
@@ -642,7 +710,7 @@ with bot:
                     link_preview=True,
                     buttons=[
                           Button.url("⚡𝗟𝘆𝗻𝘅-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡", "t.me/LynxUserbot"),
-                          Button.inline("•Oᴘᴇɴ Mᴇɴᴜ• ", data="open")
+                          Button.inline("•Oᴘᴇɴ Mᴇɴᴜ•", data="open")
                     ]
                 )
 
