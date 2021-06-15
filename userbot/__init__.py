177783a9a4e92c7911d1c23aa4e137745a219efc
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-06-15 13:46:25 +0000

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
-# Credits @keselekpermen69 / @Ultroid / @LynxUserbot
+# Credits Inline @keselekpermen69 / @Ultroid / @LynxUserbot
+# Credits Logs @BianSepang , @KenHV
 # Ported @SyndicateTwenty4
 """Userbot initialization."""
 
