183ed54f964136373849324bbc87efca9f10d8b5
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-06-15 14:45:00 +0000

    Update __init__.py
    Lol 2

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -695,15 +695,6 @@ with bot:
             "Mode Inline Bot Mu Nonaktif. "
             "Untuk Mengaktifkannya, Silahkan Pergi Ke @BotFather Lalu, Settings Bot > Pilih Mode Inline > Turn On. ")
 
-with bot:
-    try:
-        bot.loop.run_until_complete(check_botlog_chatid())
-    except BaseException:
-        LOGS.info(
-            "BOTLOG_CHATID environment variable isn't a "
-            "valid entity. Check your environment variables/config.env file.")
-        quit(1)
-
 
 async def update_restart_msg(chat_id, msg_id):
     DEFAULTUSER = ALIVE_NAME or "Set `ALIVE_NAME` ConfigVar!"
