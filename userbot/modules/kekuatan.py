32956aded91e5fe6e73e32ca861f7dfe66dbf3d6
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-06-17 12:04:17 +0000

    Update: cleanup whitespace

--- userbot/modules/kekuatan.py
+++ userbot/modules/kekuatan.py
@@ -7,19 +7,19 @@ from userbot import bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP
 
 @register(outgoing=True, pattern=r'^.kekuatan(:? |$)([1-8])?')
 async def _(fry):
-    await fry.edit("`Lord Mengaktifkan Kekuatan Telegram...👾`")
+    await fry.edit("`Lynx Mengaktifkan Kekuatan Telegram...👾`")
     level = fry.pattern_match.group(2)
     if fry.fwd_from:
         return
     if not fry.reply_to_msg_id:
-        await fry.edit("`Mohon Balas Di Sticker Lord`")
+        await fry.edit("`Mohon Balas Di Sticker.`")
         return
     reply_message = await fry.get_reply_message()
     if not reply_message.media:
-        await fry.edit("`Gambar tidak di dukung`")
+        await fry.edit("`Gambar Tidak Didukung`")
         return
     if reply_message.sender.bot:
-        await fry.edit("`Mohon Balas Di Sticker Lord`")
+        await fry.edit("`Mohon Balas Di Sticker.`")
         return
     chat = "@image_deepfrybot"
     message_id_to_reply = fry.message.reply_to_msg_id
@@ -35,13 +35,13 @@ async def _(fry):
                 response = await conv.get_response()
             else:
                 response = await conv.get_response()
-            """ - don't spam notif - """
+            """Don't spam notif"""
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await fry.reply("`Lord Mohon Unblock` @image_deepfrybot`...`")
+            await fry.reply("`Mohon Unblock` @image_deepfrybot`...`")
             return
         if response.text.startswith("Forward"):
-            await fry.edit("`Lord Mohon Matikan Setelan Forward Privasi...`")
+            await fry.edit("`Mohon Matikan Setelan Forward Privasi...`")
         else:
             downloaded_file_name = await fry.client.download_media(
                 response.media,
@@ -53,7 +53,7 @@ async def _(fry):
                 force_document=False,
                 reply_to=message_id_to_reply
             )
-            """ - cleanup chat after completed - """
+            """Cleanup chat after completed"""
             try:
                 msg_level
             except NameError:
@@ -74,14 +74,14 @@ async def _(fry):
     if fry.fwd_from:
         return
     if not fry.reply_to_msg_id:
-        await fry.edit("`Mohon Balas Di Sticker Lord`")
+        await fry.edit("`Mohon Balas Di Sticker.`")
         return
     reply_message = await fry.get_reply_message()
     if not reply_message.media:
-        await fry.edit("`Mohon Balas Di Sticker Lord`")
+        await fry.edit("`Mohon Balas Di Sticker.`")
         return
     if reply_message.sender.bot:
-        await fry.edit("`Mohon Balas Di Sticker Lord`")
+        await fry.edit("`Mohon Balas Di Sticker.`")
         return
     chat = "@image_deepfrybot"
     message_id_to_reply = fry.message.reply_to_msg_id
@@ -100,10 +100,10 @@ async def _(fry):
             """ - don't spam notif - """
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await fry.reply("`Lord Mohon Unblock` @image_deepfrybot`...`")
+            await fry.reply("`Mohon Unblock` @image_deepfrybot`...`")
             return
         if response.text.startswith("Forward"):
-            await fry.edit("`Lord Mohon Matikan Setelan Privasi Forward...`")
+            await fry.edit("`Mohon Matikan Setelan Privasi Forward...`")
         else:
             downloaded_file_name = await fry.client.download_media(
                 response.media,
@@ -115,7 +115,7 @@ async def _(fry):
                 force_document=False,
                 reply_to=message_id_to_reply
             )
-            """ - cleanup chat after completed - """
+            """Cleanup chat after completed"""
             try:
                 msg_level
             except NameError:
@@ -130,7 +130,7 @@ async def _(fry):
 
 
 CMD_HELP.update({
-    "kekuatan":
-    "`.kekuatan` or `.kekuatan` [level(1-8)]"
-    "\nUsage: untuk mengubah foto/sticker."
+    "kekuatan": "✘ Pʟᴜɢɪɴ : Kekuatan"
+    "\n\n⚡𝘾𝙈𝘿⚡: `.kekuatan` or `.kekuatan` [level(1-8)]"
+    "\n↳ : Untuk Mengubah Foto/Sticker."
 })
