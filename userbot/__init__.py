443cb3dad018f3f8ee91e00905e472a15ff6bf51
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-06-18 20:29:53 +0000

    Update: init: delete lynx text custom

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -158,9 +158,6 @@ ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
 # Youtube API key
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 
-# Untuk Perintah .lynx (alive)
-LYNX_TEXT_CUSTOM = os.environ.get("LYNX_TEXT_CUSTOM", None)
-
 # Default .alive Name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
