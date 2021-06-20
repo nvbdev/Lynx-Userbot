24a883105e98353adeff53801bfdeac148c9f36b
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-06-20 20:08:53 +0000

    Update: telegraph

--- userbot/modules/telegraph.py
+++ userbot/modules/telegraph.py
@@ -15,8 +15,7 @@ auth_url = r["auth_url"]
 async def telegraphs(graph):
     """For .telegraph command, upload media & text to telegraph site."""
     await graph.edit("`Processing...`")
-    if not graph.text[0].isalpha() and graph.text[0] not in (
-            "/", "#", "@", "!"):
+    if not graph.text[0].isalpha() and graph.text[0] not in ("/", "#", "@", "!"):
         if graph.fwd_from:
             return
         if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
@@ -29,7 +28,7 @@ async def telegraphs(graph):
                     r_message, TEMP_DOWNLOAD_DIRECTORY
                 )
                 await graph.edit(f"Downloaded to `{downloaded_file_name}`.")
-                if downloaded_file_name.endswith((".webp")):
+                if downloaded_file_name.endswith(".webp"):
                     resize_image(downloaded_file_name)
                 try:
                     media_urls = upload_file(downloaded_file_name)
@@ -39,7 +38,7 @@ async def telegraphs(graph):
                 else:
                     os.remove(downloaded_file_name)
                     await graph.edit(
-                        f"Successfully Uploaded to :\n [Telegra.ph](https://telegra.ph{media_urls[0]}).",
+                        f"Successfully Uploaded to [telegra.ph](https://telegra.ph{media_urls[0]}).",
                         link_preview=True,
                     )
             elif input_str == "t":
@@ -64,12 +63,12 @@ async def telegraphs(graph):
                     title_of_page, html_content=page_content
                 )
                 await graph.edit(
-                    "Successfully Uploaded to : \n"
+                    "Successfully uploaded to "
                     f"[telegra.ph](https://telegra.ph/{response['path']}).",
                     link_preview=True,
                 )
         else:
-            await graph.edit("`Reply to a Messages to get a Permanent telegra.ph Link.`")
+            await graph.edit("`Reply to a message to get a permanent telegra.ph link.`")
 
 
 def resize_image(image):
