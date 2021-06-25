7752cb154806e26bdeee941786f8965da5bdb1ae
KENZO-404 <latukolan404@gmail.com>
2021-06-25 18:22:46 +0000

    pylint: auto fixes
    Signed-off-by: KENZO-404 <latukolan404@gmail.com>

--- userbot/modules/tiktok.py
+++ userbot/modules/tiktok.py
@@ -3,13 +3,11 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
-# 
+#
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot import bot, CMD_HELP
 from userbot.events import register
-
-
 
 
 @register(outgoing=True, pattern="^.tiktok(?: |$)(.*)")
