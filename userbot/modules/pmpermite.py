5e6f513607773ecd8c0230ddee27f8d563c5e6b0
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-06-28 10:52:29 +0000

    .

--- userbot/modules/pmpermite.py
+++ userbot/modules/pmpermite.py
@@ -97,10 +97,10 @@ async def permitpm(event):
                         event.chat_id, from_user="me", search=UNAPPROVED_MSG, file=WARN_PIC
                     ):
                         await message.delete()
-                    await event.client.send_file(file='WARN_PIC', caption="UNAPPROVED_MSG")
+                    await event.client.send_file(file='resource/logo/LynxUserbot-Button.jpg', caption=f"{UNAPPROVED_MSG}")
                 LASTMSG.update({event.chat_id: event.text})
             else:
-                await event.client.send_file(file='WARN_PIC', caption="UNAPPROVED_MSG")
+                await event.client.send_file(file='resource/logo/LynxUserbot-Button.jpg', caption=f"{UNAPPROVED_MSG}")
                 LASTMSG.update({event.chat_id: event.text})
 
             if notifsoff:
