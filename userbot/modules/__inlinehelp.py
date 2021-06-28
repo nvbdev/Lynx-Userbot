0a0be57abedee0bb58d18443a0687dcba0cc3917
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-06-28 13:40:45 +0000

    .

--- userbot/modules/__inlinehelp.py
+++ userbot/modules/__inlinehelp.py
@@ -19,6 +19,8 @@
 import logging
 from userbot import BOT_USERNAME
 from userbot.events import register
+from telethon.errors.rpcerrorlist import BotInlineDisabledError
+
 
 
 logging.basicConfig(
@@ -28,19 +30,21 @@ logging.basicConfig(
 
 @register(outgoing=True, pattern=r"^\.helpme")
 async def yardim(event):
-    try:
-        tgbotusername = BOT_USERNAME
-        if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@LynxRobot")
-            await results[0].click(
-                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+    tgbotusername = BOT_USERNAME
+    if tgbotusername and BOT_TOKEN:
+        try:
+            results = await event.client.inline_query(
+                tgbotusername,
+                "@LynxRobot"
             )
-            await event.delete()
-        else:
-            await event.edit(
-                "`The bot doesn't work! Please set the Bot Token and Username correctly. The module has been stopped.`"
-            )
-    except Exception:
-        return await event.edit(
-            "`You cannot send inline results in this chat (caused by SendInlineBotResultRequest)`"
+        except BotInlineDisabledError:
+            return await event.edit("`Bot can't be used in inline mode.\nMake sure to turn on inline mode!`")
+        await results[0].click(
+            event.chat_id,
+            reply_to=event.reply_to_msg_id,
+            hide_via=True
         )
+        await event.delete()
+    else:
+        return await event.edit("`The bot doesn't work! Please set the Bot Token and Username correctly.`"
+                                "\n`The module has been stopped.`")
