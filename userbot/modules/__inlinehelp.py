e6d6ff8dec362653f0e8223dd03a25add4f9edae
KENZO-404 <latukolan404@gmail.com>
2021-06-28 13:41:41 +0000

    pylint: auto fixes
    Signed-off-by: KENZO-404 <latukolan404@gmail.com>

--- userbot/modules/__inlinehelp.py
+++ userbot/modules/__inlinehelp.py
@@ -22,7 +22,6 @@ from userbot.events import register
 from telethon.errors.rpcerrorlist import BotInlineDisabledError
 
 
-
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
     level=logging.WARNING)
