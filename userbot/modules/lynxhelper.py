a145f36e9ae77cce7e1c6828559a00ef273503e5
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-04 11:44:43 +0000

    Update: lynxhelper

--- userbot/modules/lynxhelper.py
+++ userbot/modules/lynxhelper.py
@@ -1,33 +1,30 @@
 """ Userbot module for other small commands. """
-from userbot import CMD_HELP, ALIVE_NAME
+from userbot import CMD_HELP, DEFAULTUSER
 from userbot.events import register
-
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
 
 
 @register(outgoing=True, pattern="^.lhelp$")
 async def usit(e):
     await e.edit(
-        f"**Halo Yang Mulia {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/SyndicateTwenty4)"
-        "\n[Repo](https://github.com/KENZO-404/Lynx-Userbot)"
+        f"**Hai {DEFAULTUSER} 🐈 Jika Anda Tidak Tau Perintah Untuk Memerintah Ku,\nKetik:** `.help` Atau Bisa Minta Bantuan Ke\n"
+        "\n📬**Developer :**
+        "\n[Telegram](t.me/TeamSecret_Kz)"
+        "\n[Dev Repo](https://github.com/KENZO-404)"
         "\n[Instagram](instagram.com/si_axeell)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
-        f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/KENZO-404/Lynx-Userbot/Lynx-Userbot/varshelper.txt)")
+        f"**Daftar Vars Untuk {DEFAULTUSER}:**\n"
+        "\nClick » [ [Lynx-VARS](https://raw.githubusercontent.com/KENZO-404/Lynx-Userbot/Lynx-Userbot/varshelper.txt) ] «")
 
 
 CMD_HELP.update({
     "lynxhelper":
-    "`.lhelp`\
-\nUsage: Bantuan Untuk Lynx-Userbot.\
-\n`.vars`\
-\nUsage: Melihat Daftar Vars."
+    "✘ Pʟᴜɢɪɴ : Lynx Helper\
+\n\n⚡𝘾𝙈𝘿⚡: `.lhelp`\
+\n↳ : Bantuan Untuk User Lynx.\
+\n\n⚡𝘾𝙈𝘿⚡: `.vars`\
+\n↳ : Melihat Daftar Vars Lynx-Userbot."
 })
