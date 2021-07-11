d16466dfb1bf3ed6d3406eb24d26c15d25f99f3c
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-11 23:11:37 +0000

    Chunks.

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -442,7 +442,7 @@ def paginate_help(page_number, loaded_modules, prefix):
                     "⋖╯Pʀᴇᴠ", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
-                    "ᴄʟᴏꜱᴇ", data="close"
+                    "ᴄʟᴏꜱᴇ", data="{}_close({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
                     "Nᴇxᴛ╰⋗", data="{}_next({})".format(prefix, modulo_page)
@@ -601,7 +601,11 @@ with bot:
                 reply_pop_up_alert = f"🚫!WARNING!🚫 Jangan Menggunakan Milik {DEFAULTUSER}."
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-        @callback("close")
+        @tgbot.on(
+            events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+                data=re.compile(rb"helpme_close\((.+?)\)")
+            )
+        )
         async def on_plug_in_callback_query_handler(event):
             if event.query.user_id == uid:  # Lynx-Openeer
                 # https://t.me/TelethonChat/115200
