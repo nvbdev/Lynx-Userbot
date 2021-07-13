d2377db17b9a12bbe6fe59a33081bea85c225447
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-13 05:19:20 +0000

    .

---
+++ resource/startup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Lynx-Userbot
+# Copyright © 2021 Lynx-Userbot
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+
+echo "
+           ┏┓╋┏┓╋╋┏┳━┓╋┏┳━┓┏━┓
+           ┃┃╋┃┗┓┏┛┃┃┗┓┃┣┓┗┛┏┛
+           ┃┃╋┗┓┗┛┏┫┏┓┗┛┃┗┓┏┛
+           ┃┃╋┏╋┓┏┛┃┃┗┓┃┃┏┛┗┓
+           ┃┗━┛┃┃┃╋┃┃╋┃┃┣┛┏┓┗┓
+           ┗━━━┛┗┛╋┗┛╋┗━┻━┛┗━┛
+
+       Thanks for Support @LynxUserbot
+"
+
+python3 -m userbot
