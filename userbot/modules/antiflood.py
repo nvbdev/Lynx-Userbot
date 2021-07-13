7f6b7d1a7595c095de4ab02b0be746b6a87cb8a3
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-13 18:27:22 +0000

    .

--- userbot/modules/antiflood.py
+++ userbot/modules/antiflood.py
@@ -31,7 +31,7 @@ ANTI_FLOOD_WARN_MODE = ChatBannedRights(
 async def _(event):
     if not CHAT_FLOOD:
         return
-    admin_c = await is_admins(event.client, event.chat_id, event.message.from_id)
+    admin_c = await is_admin(event.client, event.chat_id, event.message.from_id)
     if not admin_c:
         return
     if str(event.chat_id) not in CHAT_FLOOD:
@@ -49,8 +49,8 @@ async def _(event):
         no_admin_privilege_message = await event.client.send_message(
             entity=event.chat_id,
             message=f"""**Automatic AntiFlooder**
-                    @admin \n[👤USER](tg://user?id={}) is Flooding This Chat.
-                    `{}`""".format(event.message.from_id, str(e)), reply_to=event.message.id
+@admin \n[👤USER](tg://user?id={}) is Flooding This Chat.
+`{}`""".format(event.message.from_id, str(e)), reply_to=event.message.id
         )
         await asyncio.sleep(4)
         await no_admin_privilege_message.edit(
@@ -60,9 +60,9 @@ async def _(event):
         await event.client.send_message(
             entity=event.chat_id,
             message=f"""**Automatic AntiFlooder**
-                    [👤USER](tg://user?id={}) has been automatically restricted
-                    because he reached the defined flood limit.""".format(event.message.from_id),
-            reply_to=event.message.id
+[👤USER](tg://user?id={}) has been automatically restricted
+because he reached the defined flood limit.""".format(event.message.from_id),
+            reply_to=event.message.id,
         )
 
 
