43eabb653b8a1423062b0381064e6dfcf412b7dc
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-14 03:51:14 +0000

    .

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -334,6 +334,20 @@ with bot:
             api_hash=API_HASH).start(
             bot_token=BOT_TOKEN)
 
+    except BaseException:
+        LOGS.info(
+            "Mode Inline Bot Mu Nonaktif. "
+            "Untuk Mengaktifkannya, Silahkan Pergi Ke @BotFather Lalu, Settings Bot > Pilih Mode Inline > Turn On."
+        )
+    try:
+        bot.loop.run_until_complete(check_botlog_chatid())
+    except BaseException:
+        LOGS.info(
+            "BOTLOG_CHATID environment variable isn't a "
+            "valid entity. Check your environment variables/config.env file."
+        )
+        quit(1)
+
 
 async def check_botlog_chatid():
     if not BOTLOG_CHATID and LOGSPAMMER:
