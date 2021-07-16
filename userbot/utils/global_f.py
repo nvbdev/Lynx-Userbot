8695cf1312bfd7d5357634634047bf640fee3356
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-16 04:36:06 +0000

    .

--- userbot/utils/global_f.py
+++ userbot/utils/global_f.py
@@ -1,8 +1,17 @@
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+
+if STRING_SESSION:
+    # pylint: disable=invalid-name
+    bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+else:
+    # pylint: disable=invalid-name
+    bot = TelegramClient("userbot", API_KEY, API_HASH)
+
 # --------------------------
-global george
 george = bot
 
 # --------------------------
 global zeus
 zeus = page_number
-
