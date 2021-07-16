8216382c94dd5ff93d65cb78a0b40bfcbe4384c4
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-16 03:05:04 +0000

    .

---
+++ userbot/utils/global_f.py
@@ -0,0 +1,11 @@
+# --------------------------
+def george():
+    global lynx
+    lynx = bot
+    return lynx
+
+# --------------------------
+def zeus():
+    global unpage
+    unpage = page_number
+    return unpage
