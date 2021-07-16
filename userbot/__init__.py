a438e6b1b0a57b53c52cfeb32e3618730b3d546d
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-16 05:39:54 +0000

    .

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -579,11 +579,10 @@ with lynx:
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.photo(
                     file=lynxlogo,
-                    link_preview=True,
+                    link_preview=False,
                     text=f"\n**Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n\n◎› **Bᴏᴛ ᴠᴇʀ :** `v.{BOT_VER}`\n◎› **Pʟᴜɢɪɴꜱ :** `{len(plugins)}`\n\n**Cᴏᴘʏʀɪɢʜᴛ © 𝟤𝟢𝟤𝟣 Lʏɴx-Uꜱᴇʀʙᴏᴛ**".format(
                         len(dugmeler),
                     ),
-                    media=True,
                     buttons=buttons,
                 )
             elif query.startswith("tb_btn"):
@@ -607,7 +606,7 @@ with lynx:
                                 "t.me/FederationSuperGroup/17")]],
                     link_preview=True,
                 )
-            await event.answer([result] if result else None, gallery=True)
+            await event.answer([result] if result else None, )
 
         @lynx.tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -620,8 +619,11 @@ with lynx:
             await event.edit(
                 file=lynxlogo,
                 buttons=buttons,
-                link_preview=True,
+                link_preview=False,
             )
+            else:
+                reply_pop_up_alert = f"🚫!WARNING!🚫\nJangan Menggunakan Milik {DEFAULTUSER}."
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
         @lynx.tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -630,10 +632,7 @@ with lynx:
         )
         async def on_plug_in_callback_query_handler(event):
             if event.query.user_id == uid:
-                await event.edit(f"🕹 **<--- • Menu Has Closed • --->** 🕹", file=lynxlogo)
-            else:
-                reply_pop_up_alert = f"🚫!WARNING!🚫\nJangan Menggunakan Milik {DEFAULTUSER}."
-                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+                await event.edit(f"🕹 **<--- • Menu Has Closed • --->** 🕹", file=lynxlogo, buttons=Button.clear())
 
         @lynx.tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
