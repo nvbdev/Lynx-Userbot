ee3b7ecc6142c44d5386b5b603d208b8fd484371
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-16 04:44:23 +0000

    Update global_f.py

--- userbot/utils/global_f.py
+++ userbot/utils/global_f.py
@@ -1,13 +1,4 @@
-from telethon import TelegramClient
-from telethon.sessions import StringSession
-
-
-if STRING_SESSION:
-    # pylint: disable=invalid-name
-    bot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
-else:
-    # pylint: disable=invalid-name
-    bot = TelegramClient("userbot", API_KEY, API_HASH)
+from userbot import bot
 
 # --------------------------
 george = bot
