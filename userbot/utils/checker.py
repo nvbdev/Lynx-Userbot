87475fa7d20c3934247fc569fec00b42d23943d1
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-17 09:26:34 +0000

    Update checker.py

--- userbot/utils/checker.py
+++ userbot/utils/checker.py
@@ -1,13 +1,13 @@
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
-
+from userbot import bot
 from ..core.logger import logging
 
 LOGS = logging.getLogger(__name__)
 
 
 # Admin checker by uniborg
-async def is_admin(chat_id, user_id):
+async def is_admin(bot, chat_id, user_id):
     if not str(chat_id).startswith("-100"):
         return False
     try:
