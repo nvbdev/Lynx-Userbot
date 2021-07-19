f606bc27dcacde42196b4aa535a300e62d55d62a
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-19 15:07:48 +0000

    Update tiktok.py

--- userbot/modules/tiktok.py
+++ userbot/modules/tiktok.py
@@ -15,7 +15,7 @@ async def _(event):
     if event.fwd_from:
         return
     d_link = event.pattern_match.group(1)
-    if "vm." or ".com" not in d_link:
+    if ".com" not in d_link:
         await event.edit("`Mohon Maaf, Saya Membutuhkan Link Video Tiktok Untuk Mendownload Nya` 🤬")
     else:
         await event.edit("```Video Sedang Diproses...```")
