af3ead6202daea17086c497782c6dbcf22782bd5
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-22 01:29:58 +0700

    Update: create group & channel
    • CMD Text.

--- userbot/modules/create.py
+++ userbot/modules/create.py
@@ -1,55 +1,77 @@
-# lorduserbot
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
 from telethon.tl import functions
 from userbot.events import register
 from userbot import CMD_HELP
 
 
-@register(outgoing=True, pattern="^.buat (gb|g|c)(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.create (b|g|c)(?: |$)(.*)")
 async def telegraphs(grop):
-    """ For .create command, Creating New Group & Channel """
-    if not grop.text[0].isalpha() and grop.text[0] not in ("/", "#", "@", "!"):
-        if grop.fwd_from:
-            return
-        type_of_group = grop.pattern_match.group(1)
-        group_name = grop.pattern_match.group(2)
-        if type_of_group == "gb":
-            try:
-                result = await grop.client(functions.messages.CreateChatRequest(  # pylint:disable=E0602
+    """For .create command, Creating New Group & Channel"""
+    if grop.text[0].isalpha() or grop.text[0] in ("/", "#", "@", "!"):
+        return
+    if grop.fwd_from:
+        return
+    type_of_group = grop.pattern_match.group(1)
+    group_name = grop.pattern_match.group(2)
+    if type_of_group == "b":
+        try:
+            result = await grop.client(
+                functions.messages.CreateChatRequest(  # pylint:disable=E0602
                     users=["@MissRose_bot"],
                     # Not enough users (to create a chat, for example)
                     # Telegram, no longer allows creating a chat with ourselves
-                    title=group_name
-                ))
-                created_chat_id = result.chats[0].id
-                result = await grop.client(functions.messages.ExportChatInviteRequest(
-                    peer=created_chat_id,
-                ))
-                await grop.edit("Grup/Channel {} Berhasil Dibuat. Tekan [{}]({}) Untuk Melihatnya".format(group_name, group_name, result.link))
-            except Exception as e:  # pylint:disable=C0103,W0703
-                await grop.edit(str(e))
-        elif type_of_group == "g" or type_of_group == "c":
-            try:
-                r = await grop.client(functions.channels.CreateChannelRequest(  # pylint:disable=E0602
                     title=group_name,
-                    about="`Selamat Datang Di Channel Ini!`",
-                    megagroup=False if type_of_group == "c" else True
-                ))
-                created_chat_id = r.chats[0].id
-                result = await grop.client(functions.messages.ExportChatInviteRequest(
+                )
+            )
+            created_chat_id = result.chats[0].id
+            result = await grop.client(
+                functions.messages.ExportChatInviteRequest(
                     peer=created_chat_id,
-                ))
-                await grop.edit("Grup/Channel {} Berhasil Dibuat. Tekan [{}]({}) Untuk Melihatnya".format(group_name, group_name, result.link))
-            except Exception as e:  # pylint:disable=C0103,W0703
-                await grop.edit(str(e))
+                )
+            )
+            await grop.edit(
+                "Your {0} Group Created Successfully. Click [{0}]({1}) to join".format(
+                    group_name, result.link
+                )
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await grop.edit(str(e))
+    elif type_of_group in ("g", "c"):
+        try:
+            r = await grop.client(
+                functions.channels.CreateChannelRequest(  # pylint:disable=E0602
+                    title=group_name,
+                    about="Welcome to this Channel",
+                    megagroup=not bool(type_of_group == "c"),
+                )
+            )
+            created_chat_id = r.chats[0].id
+            result = await grop.client(
+                functions.messages.ExportChatInviteRequest(
+                    peer=created_chat_id,
+                )
+            )
+            await grop.edit(
+                "Your {0} Group/Channel Created Successfully. Click [{0}]({1}) to join".format(
+                    group_name, result.link
+                )
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await grop.edit(str(e))
+
 
 CMD_HELP.update({
-    "membuat": "\
-Membuat\
-\nPenggunaan : Untuk membuat Channel, Grup dan Grup bersama Bot.\
-\n\n⚡𝘾𝙈𝘿⚡: `.buat g` <Nama Group>\
+    "create": "✘ Pʟᴜɢɪɴ : Create a Group\
+\nPenggunaan : Untuk Membuat Channel, Group dan Group Bersama Bot.\
+\n\n⚡𝘾𝙈𝘿⚡: `.create g` <Nama Group>\
 \n↳ : Membuat Group Mu.\
-\n\n⚡𝘾𝙈𝘿⚡: `.buat gb` <Nama Group>\
+\n\n⚡𝘾𝙈𝘿⚡: `.create b` <Nama Group>\
 \n↳ : Membuat Group Bersama Dengan Bot.\
-\n\n⚡𝘾𝙈𝘿⚡: `.buat c` <Nama Channel>\
+\n\n⚡𝘾𝙈𝘿⚡: `.create c` <Nama Channel>\
 \n↳ : Membuat Sebuah Channel.\
 "})
