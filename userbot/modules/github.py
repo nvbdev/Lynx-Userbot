81ae8312ae68b8dcdba6350f40c2736fdaf8a84a
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-22 02:40:35 +0700

    Update: github.
    Rgx

--- userbot/modules/github.py
+++ userbot/modules/github.py
@@ -4,7 +4,7 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(pattern=r".git (.*)", outgoing=True)
+@register(pattern=r"^\.git (.*)", outgoing=True)
 async def github(event):
     URL = f"https://api.github.com/users/{event.pattern_match.group(1)}"
     await event.get_chat()
