d3665bcd2b4275fd6bba12a48d07a857a5013339
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-21 01:37:41 +0000

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -115,8 +115,8 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/KENZO-404/Lynx-Userbot")
-UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Lynx-Userbot")
+    "https://github.com/KENZO-404/Lynx-Userbot/tree/Beta")
+UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Beta")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
