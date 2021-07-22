9738e57707d574053bbc7e7c87f1d648bbbfa9c7
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-22 20:43:02 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -116,7 +116,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/KENZO-404/Lynx-Userbot")
-UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Beta")
+UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Lynx-Userbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
